6fc6765e75f59643267936da177117f13facdd01
Todor Mihaylov <todormihaylov95@gmail.com>
2017-05-02 17:39:45 +0300

    Add files via upload

---
+++ 04.C# Unit Testing/01.Unit-Testing - Homework/01.StudentsAndCourses/School.Tests/CourseTests.cs
@@ -0,0 +1,69 @@
+﻿namespace School.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Models;
+
+    [TestClass]
+    public class CourseTests
+    {
+        [TestMethod]
+        public void Course_ShouldCreateACourse_IfConstructorIsWorkingProperly()
+        {
+            var course = new Course();
+
+            Assert.IsNotNull(course);
+        }
+
+        [TestMethod]
+        public void Course_ShouldAddStudent_IfAddStudentIsWorkingProperly()
+        {
+            var course = new Course();
+            var student = new Student("Gosho");
+            var expectedStudentCount = 1;
+
+            course.AddStudent(student);
+            var actualStudentCount = course.Students.Count;
+
+            Assert.AreEqual(expectedStudentCount, actualStudentCount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Course_ShouldThrowAnException_IfAddStudentWhenCourseIsFull()
+        {
+            var course = new Course();
+            var student = new Student("Gosho");
+
+            for (int i = 0; i < 31; i++)
+            {
+                course.AddStudent(student);
+            }
+        }
+
+        [TestMethod]
+        public void Course_ShouldRemoveStudent_IfRemoveStudentIsWorkingProperly()
+        {
+            var course = new Course();
+            var student = new Student("Gosho");
+            var expectedStudentCount = 0;
+
+            course.AddStudent(student);
+            course.RemoveStudent(student);
+            var actualStudentCount = course.Students.Count;
+
+            Assert.AreEqual(expectedStudentCount, actualStudentCount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Course_ShouldThrowAnException_IfRemoveStudentWhenCourseIsEmpty()
+        {
+            var course = new Course();
+            var student = new Student("Gosho");
+
+            course.RemoveStudent(student);
+        }
+    }
+}
---
+++ 04.C# Unit Testing/01.Unit-Testing - Homework/01.StudentsAndCourses/School/Models/Student.cs
@@ -0,0 +1,40 @@
+﻿namespace School.Models
+{
+    using Utils;
+
+    public class Student
+    {
+        private string name;
+        private int id;
+
+        public Student(string name)
+        {
+            this.Name = name;
+            this.Id = IdGenerator.GenerateId();
+        }
+
+        public string Name {
+            get
+            {
+                return this.name;
+            }
+            protected set
+            {
+                Validator.ValidateNullOrEmpty(value, Constants.StudentNameCannotBeNull);
+                this.name = value;
+            }
+        }
+
+        public int Id {
+            get
+            {
+                return this.id;
+            }
+            private set
+            {
+                Validator.ValidateIntRange(value, Constants.MinimumIdNumber, Constants.MaximumIdNumber, Constants.IdOutOfRange);
+                this.id = value;
+            }
+        }
+    }
+}
