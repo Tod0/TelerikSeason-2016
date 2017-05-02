9f202fe015143872840124efac4f31468bac3a2d
Todor Mihaylov <todormihaylov95@gmail.com>
2017-05-02 13:34:06 +0300

    Add files via upload

---
+++ C#/01.C# Part 1/01.Intro-Programming - Homework/Age/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your birthday: ");
+            DateTime birthday = DateTime.Parse(Console.ReadLine());
+            DateTime today = DateTime.Now;
+            int age = today.Year - birthday.Year;
+            if (today < birthday.AddYears(age))
+                age--;
+            Console.WriteLine("Your age is: " + age);
+            Console.WriteLine("Your age after 10 years will be: " + (age + 10));   
+        }
+    }
+}
---
+++ C#/01.C# Part 1/02.DataTypesAndVariables - Homework/QuotesInStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QuotesInStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "The \"use\" of quotations causes difficulties.";
+
+            Console.WriteLine(a);
+        }
+    }
+}
